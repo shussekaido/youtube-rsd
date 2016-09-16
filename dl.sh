875bc22942a95e18ef5da318f87c6b0d968a76bf
shussekaido <shussekaido@gmail.com>
2016-09-16 20:01:02 +0900

    Changed youtube-dl audio format from bestaudio to 140 due to a youtube-dl issue: https://github.com/rg3/youtube-dl/issues/3721

--- dl.sh
+++ dl.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-youtube-dl -o '%(uploader)s/%(upload_date)s - %(title)s.%(ext)s' -f bestaudio --batch-file 'config/urls.txt' --download-archive 'config/history.txt' --exec 'config/process.sh {}'
+youtube-dl -o '%(uploader)s/%(upload_date)s - %(title)s.%(ext)s' -f 140 --batch-file 'config/urls.txt' --download-archive 'config/history.txt' --exec 'config/process.sh {}'
